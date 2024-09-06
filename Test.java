d694699eca042273bc345969fc54d784f5d9e24f
earl-1b <eal5801@students.uc-bcf.edu.ph>
2024-09-06 08:48:28 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author m303user
+ */
+/*Anna
+September 6,2024
+First Programming Session*/
+public class Test {
+    public static void main(String [] args){
+        System.out.println("ma,am Anna \t\tRhodora \nQuitaleg");
+        //Java Escape characters
+        System.out.print("\n\n\nAnna\"s Class");
+        System.out.println("i Love Java programming");
+    }
+}
